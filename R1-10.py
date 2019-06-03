9440874dbc987d17709954bb87dd21979661a565
Carlos Hernandez <carloshndz.us@gmail.com>
2019-06-02 20:34:00 -0500

    Exerice completed

---
+++ R1-10.py
@@ -0,0 +1,9 @@
+# What parameters should be sent to the range constructor, to produce a range
+# with values 8,6,4,2,0,-2,-4,-6,-8?
+
+# start should be 8
+# stop should be -10
+# step should be -2
+
+for k in range(8, -10, -2):
+    print(k)
