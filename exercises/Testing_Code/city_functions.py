1988e2b4a6c2f90aa47e9cf4c9becf2e59df0522
Carlos Hernandez <carloshndz.us@gmail.com>
2019-09-26 08:51:16 -0500

    Added population parameter to my previous file and added additional unit tests to test new parameter.

--- exercises/Testing_Code/city_functions.py
+++ exercises/Testing_Code/city_functions.py
@@ -1,5 +1,8 @@
-def get_formatted_name(city, country):
+def get_formatted_name(city, country, population=''):
     """Generate a neatly formatted sring of the form 'City, Country'."""
-    formatted_name = city.title() + ', ' + country.title()
+    if population:
+        formatted_name = city.title() + ', ' + country.title() + ' - Population ' + str(population)
+    else:
+        formatted_name = city.title() + ', ' + country.title()
     return formatted_name
 
--- exercises/Testing_Code/test_cities.py
+++ exercises/Testing_Code/test_cities.py
@@ -4,9 +4,14 @@ from city_functions import get_formatted_name
 class TestCityNames(unittest.TestCase):
     """Tests for 'city_functions.py'."""
 
-    def test_city_names(self):
+    def test_city_country(self):
         """Does city names like 'Santiago, Chile' work?"""
         formatted_name = get_formatted_name('santiago', 'chile')
         self.assertEqual(formatted_name, "Santiago, Chile")
+
+    def test_city_country_population(self):
+        """Does city, country, and population like "Santiago, Chile - Population 5000000" work?"""
+        formatted_name = get_formatted_name('santiago', 'chile', 5000000)
+        self.assertEqual(formatted_name, "Santiago, Chile - Population 5000000")
 
 unittest.main()
